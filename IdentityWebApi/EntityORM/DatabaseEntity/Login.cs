9a9a3b122d32281a1f68046af42d75028caf4c3c
Aditya <abeysingheaditya4@gmail.com>
2023-12-19 23:05:32 +0530

    Added EF core context classes and endpoint logic

---
+++ IdentityWebApi/EntityORM/DatabaseEntity/Login.cs
@@ -0,0 +1,16 @@
+﻿namespace EntityORM.DatabaseEntity;
+
+public partial class Login
+{
+    public string Username { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public DateTime RegisteredDate { get; set; }
+
+    public DateTime LastLoginTime { get; set; }
+
+    public string? UserRole { get; set; }
+
+    public bool IsActive { get; set; }
+}
