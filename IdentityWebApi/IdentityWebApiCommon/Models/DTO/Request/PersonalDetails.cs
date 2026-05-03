aab89ed9c610b032478fe4e7ce2312fe2ac7c0eb
Aditya <abeysingheaditya4@gmail.com>
2026-05-03 22:48:24 +0530

    Added model for PersonalDetails

---
+++ IdentityWebApi/IdentityWebApiCommon/Models/DTO/Request/PersonalDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace IdentityWebApiCommon.Models.DTO.Request
+{
+    public class PersonalDetails
+    {
+        public string Location { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Status { get; set; }
+
+        public string Gender { get; set; }
+
+        public string LanguageOne { get; set; }
+
+        public string LanguageTwo { get; set; }
+    }
+}
