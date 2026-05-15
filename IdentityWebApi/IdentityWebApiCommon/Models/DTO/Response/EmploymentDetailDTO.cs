aaffc6336d990edf4e8da76eac37c46d7b52d157
Aditya <abeysingheaditya4@gmail.com>
2026-05-15 23:17:37 +0530

    Fixed issue of SaveUserData

--- IdentityWebApi/IdentityWebApiCommon/Models/DTO/Response/EmploymentDetailDTO.cs
+++ IdentityWebApi/IdentityWebApiCommon/Models/DTO/Response/EmploymentDetailDTO.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Text.Json.Serialization;
-
 namespace IdentityWebApiCommon.Models.DTO.Response
 {
     public class EmploymentDetailDTO
     {
-        [JsonIgnore]
         public int Id { get; set; }
-        [JsonIgnore]
         public int UserId { get; set; }
         public string? EmployerName { get; set; }
         public string? EmployerCity { get; set; }
