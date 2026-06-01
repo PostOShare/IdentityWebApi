150fa8819b245f611cc1f925ccbea4718085a979
Aditya <abeysingheaditya4@gmail.com>
2026-06-01 22:59:03 +0530

    Added JsonProperty name values to user endpoints

--- IdentityWebApi/IdentityWebApiCommon/Models/DTO/Request/SaveUserRequestDTO.cs
+++ IdentityWebApi/IdentityWebApiCommon/Models/DTO/Request/SaveUserRequestDTO.cs
@@ -1,23 +1,31 @@
 ﻿using IdentityWebApiCommon.Models.DTO.Response;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace IdentityWebApiCommon.Models.DTO.Request
 {
+    [JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Disallow)]
     public class SaveUserRequestDTO
     {
         [Required]
+        [JsonPropertyName("username")]
         public string Username { get; set; } = null!;
 
         [Required]
+        [JsonPropertyName("refreshToken")]
         public string? RefreshToken { get; set; }
 
         [Required]
+        [JsonPropertyName("accessToken")]
         public string? AccessToken { get; set; }
 
+        [JsonPropertyName("personalDetail")]
         public PersonalDetailDTO? PersonalDetail { get; set; }
 
+        [JsonPropertyName("employmentDetail")]
         public List<EmploymentDetailDTO>? EmploymentDetail { get; set; }
 
+        [JsonPropertyName("learnDetail")]
         public List<LearnDetailDTO>? LearnDetail { get; set; }
     }
 }
